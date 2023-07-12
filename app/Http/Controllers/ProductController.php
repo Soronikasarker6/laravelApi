8c750b5a378a6c39da89840486b42918917e3c2a
soronikasarkerpinkey <spinkey@schertech.com>
2023-07-12 10:08:34 +0600

    handle exception

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,7 @@ use App\Models\Product;
 use PharException;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
+use Flat3\Lodata\Controller\Response;
 
 class ProductController extends Controller
 {
@@ -37,7 +38,7 @@ class ProductController extends Controller
                 !isset($request->name) || !isset($request->detail) ||
                 !isset($request->price) || !isset($request->stock)
             ) {
-                return response()->json(['error' => 'Send all required parameters'], 422);
+                return response()->json(['error' => 'Send all required parameters'], Response::HTTP_UNPROCESSABLE_ENTITY);
             }
 
             // Retrieve the data from the request
@@ -59,9 +60,9 @@ class ProductController extends Controller
             $baseData->save();
 
             // Return a response
-            return response()->json(['message' => 'Post created successfully', 'data' => $baseData], 201);
+            return response()->json(['message' => 'Post created successfully', 'data' => $baseData], Response::HTTP_OK);
         } catch (PharException $e) {
-            abort(500, 'An error occurred.');
+            abort(Response::HTTP_ACCEPTED, 'An error occurred.');
         }
     }
 
@@ -87,15 +88,19 @@ class ProductController extends Controller
      */
     public function update(UpdateProductRequest $request, $id)
     {
+        // $product->update($request->all());
+
         $showbase = Product::findOrFail($id);
         $showbase->employee_nr = $request->input('name');
         $showbase->employee_name = $request->input('detail');
         $showbase->user_name = $request->input('price');
         $showbase->company = $request->input('stock');
 
+
         $result = $showbase->update();
+        
         if ($result) {
-            return response()->json($showbase);
+            return response()->json(['message' => 'Post created successfully', 'data' => $result], Response::HTTP_OK);
         } else {
             return ["Not updated data"];
         }
@@ -104,16 +109,10 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy($id)
+    public function destroy(Product $product)
     {
-        $showbase = Product::find($id);
-        $result = $showbase->delete();
-        if ($result) {
-            return [
-                "result" => "Delete Successful"
-            ];
-        } else {
-            return ["Not deleted data"];
-        }  
+       $product->delete();
+       return response()->json(['message' => 'Deleted successfully', 'data' => $product], Response::HTTP_NO_CONTENT);
+    //    return response([null, Response::HTTP_NO_CONTENT])
     }
 }
